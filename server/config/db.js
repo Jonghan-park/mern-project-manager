66463b015bf27aeda702a3d37ca3c1c39445738b
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-18 11:03:16 -0600

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,2 +1,9 @@
 const mongoose = require('mongoose');
 
+const connectDB = async () => {
+  const conn = await mongoose.connect(precess.env.MONGO_URI,{
+    
+  })
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+}
