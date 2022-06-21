63c1dabe326653aa879ffb369b9ca1df2627fb85
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-20 21:21:12 -0600

    Update clients

---
+++ client/src/components/Clients.js
@@ -0,0 +1,31 @@
+import { gql, useQuery } from '@apollo/client'
+
+const GET_CLIENTS = gql`
+  query getClients{
+    clients{
+      id
+      name
+      email
+      phone
+    }
+  }
+`
+
+export default function Clients() {
+  const { loading, error, data } = useQuery(GET_CLIENTS)
+
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>Something went wrong</p>
+
+  return (
+    <>
+      {!loading && !error && (
+        (
+          <table className="table table-hover mt-3">
+            
+          </table>
+        )
+      )}
+    </>
+  )
+}
