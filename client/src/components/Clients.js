83fa0d6d6693a8422e814fa399909fa77e02e399
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-20 21:50:49 -0600

    Update clients

--- client/src/components/Clients.js
+++ client/src/components/Clients.js
@@ -1,15 +1,6 @@
 import { gql, useQuery } from '@apollo/client'
-
-const GET_CLIENTS = gql`
-  query getClients{
-    clients{
-      id
-      name
-      email
-      phone
-    }
-  }
-`
+import ClientRow from './ClientRow'
+import { GET_CLIENTS } from '../queries/clientQueries'
 
 export default function Clients() {
   const { loading, error, data } = useQuery(GET_CLIENTS)
@@ -22,7 +13,18 @@ export default function Clients() {
       {!loading && !error && (
         (
           <table className="table table-hover mt-3">
-            
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.clients.map(client => (
+                <ClientRow key={client.id} client={client} />
+              ))}
+            </tbody>
           </table>
         )
       )}
