25c11949adf1fa21c65e4faa732734cef7b8c64b
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-20 23:00:29 -0600

    Update addclient

--- client/src/components/AddClientModal.js
+++ client/src/components/AddClientModal.js
@@ -3,6 +3,15 @@ import { FaUser } from 'react-icons/fa'
 import { useMutation } from '@apollo/client'
 
 export default function AddClientModal() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, phone);
+  }
+
   return (
     <>
     <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#addClientModal">
@@ -16,16 +25,28 @@ export default function AddClientModal() {
   <div className="modal-dialog">
     <div className="modal-content">
       <div className="modal-header">
-        <h5 className="modal-title" id="addClientModalLabel">Modal title</h5>
+        <h5 className="modal-title" id="addClientModalLabel">Add Client</h5>
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div className="modal-body">
-        ...
+        <form onSubmit={onSubmit}>
+          <div className="mb-3">
+            <label className='form-label'>Name</label>
+              <input type="text" className="form-control" id='name' value={name} onChange={(e) => setName(e.target.value)} />
+          </div>
+          <div className="mb-3">
+            <label className='form-label'>Email</label>
+              <input type="email" className="form-control" id='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className="mb-3">
+            <label className='form-label'>Phone</label>
+              <input type="text" className="form-control" id='phone' value={phone} onChange={(e) => setPhone(e.target.value)} />
+          </div>
+
+          <button type='submit' data-bs-dismiss="modal" className='btn btn-secondary'>Submit</button>
+        </form>
       </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
-      </div>
+      
     </div>
   </div>
 </div>
