669787b8482334ef9f92422652f94712d248dcfa
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-23 08:16:16 -0600

    Update project mutation

---
+++ client/src/mutations/projectMutation.js
@@ -0,0 +1,16 @@
+import { gql} from '@apollo/client'
+
+const ADD_PROJECT = gql`
+  mutation AddProject($name: String!, $description: String!, $status: ProjectStatus!, description: $description, status: $status, clientId: $clientId){
+    id
+    name
+    description
+    status
+    client{
+      id
+      name
+      email
+      phone
+    }
+  }
+`
