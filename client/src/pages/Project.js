fbf17790825841fe437178aae483066549c6e492
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-23 21:33:03 -0600

    Update delete project button

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -3,6 +3,7 @@ import { Link, useParams } from 'react-router-dom'
 import ClientInfo from '../components/ClientInfo';
 import Spinner from '../components/Spinner'
 import { GET_PROJECT } from '../queries/projectQueries';
+import DeleteProjectButton from '../components/DeleteProjectButton'
 
 export default function Project() {
   const {id} = useParams();
@@ -24,6 +25,8 @@ export default function Project() {
           <p className='lead'>{data.project.status}</p>
 
           <ClientInfo client={data.project.client} />
+
+          <DeleteProjectButton projectId={data.project.id} />
         </div>
       ) }
     </>
