6be9322d757745936da827325c0e1e5882287438
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-25 21:24:21 -0600

    heroku deployment

--- server/index.js
+++ server/index.js
@@ -14,6 +14,12 @@ connectDB();
 
 app.use(cors());
 
+app.use(express.static("client/build"));
+
+app.get("*", (req,res) => {
+  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+})
+
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: process.env.NODE_ENV === 'production'
