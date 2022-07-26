a8e0c33a84b1e048563b03f0761ebbc36092afb5
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-26 17:44:49 -0600

    Update server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const colors = require('colors');
-const cors = require('cors')
-require('dotenv').config();
-const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema');
-const connectDB = require('./config/db')
+const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
-const path = require('path')
+const path = require("path");
 
 const app = express();
 
@@ -15,20 +15,28 @@ connectDB();
 
 app.use(cors());
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static("../client/build"));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  });
 
-app.get("*", (req,res) => {
-  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
-})
+  // app.use(express.static("../client/build"));
+
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  // });
+  //
 }
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: process.env.NODE_ENV === 'production'
-}))
-
-
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "production",
+  })
+);
 
 console.log(__dirname);
 
